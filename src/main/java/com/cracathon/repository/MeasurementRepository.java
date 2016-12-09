cded0470b873c87ec21b144c401e00e5a6079a73
Mateisz Brycki <mateusz.brycki@outlook.com>
2016-12-09 22:41:11 +0100

    master - PositionDataService

--- src/main/java/com/cracathon/repository/MeasurementRepository.java
+++ src/main/java/com/cracathon/repository/MeasurementRepository.java
@@ -2,9 +2,11 @@ package com.cracathon.repository;
 
 import com.cracathon.domain.Measurement;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Michał on 2016-12-09.
  */
-public interface MeasureMentRepository extends CrudRepository<Measurement, Long> {
+@Repository
+public interface MeasurementRepository extends CrudRepository<Measurement, Long> {
 }
