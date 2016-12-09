4bc5c5ca6d359e8a221b713e734d765d20240256
winio94 <winio94@gmail.com>
2016-12-09 20:30:40 +0100

    initial commit

---
+++ src/main/java/com/cracathon/Application.java
@@ -0,0 +1,14 @@
+package com.cracathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Michał on 2016-12-09.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
